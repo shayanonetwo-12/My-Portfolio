e4a85d916afb3f677fe39eb9b3e7b62987dcac6b
Shayan Shahid <shayanonetwo@gmail.com>
2026-08-05 14:31:20 +0500

    Remove base path configuration in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
